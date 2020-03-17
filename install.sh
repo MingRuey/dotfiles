4217872b2a5dbd3ad47217f8beb91ad0d4f2c88c
mrchou <imchou239@gmail.com>
2020-03-17 17:16:08 +0800

    make apt-get install auto

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 aptgetPKGS="zsh zsh-doc autojump"
 
 echo "Check if $aptgetPKGS installed:"
-sudo apt-get install $aptgetPKGS
+sudo apt-get install -y $aptgetPKGS
 
 echo -n "Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
