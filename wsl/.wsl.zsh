3d0bb273291b2fbe53e9821d50ed94d17a468bd6
mrchou <imchou239@gmail.com>
2020-07-24 15:34:21 +0800

    [What] add vim-surround for VsVim (vim emulator of Visual Studio), which mimics VS-Code Vim

--- wsl/.wsl.zsh
+++ wsl/.wsl.zsh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "Setting WSL Zsh settings"
+
+# copy vimrc file for VsVim (vim emulator for Visual Studio)
+WINUSERP=$(wslpath $(wslvar USERPROFILE))
+cp ./.wsl.vsvimrc $WINUSERP/_vsvimrc
