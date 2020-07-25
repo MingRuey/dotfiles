55cc0ea1408b2169dbebcc7389f7f43b5f74642f
mrchou <imchou239@gmail.com>
2020-07-25 21:07:04 +0800

    [What] fix .wsl.vsvimrc copy path

--- wsl/.wsl.zsh
+++ wsl/.wsl.zsh
@@ -4,5 +4,6 @@ echo "Setting WSL Zsh settings"
 
 # copy vimrc file for VsVim (vim emulator for Visual Studio)
 WINUSERP=$(wslpath $(wslvar USERPROFILE))
-find . -name ".wsl.vsvimrc" -exec cp {} $WINUSERP/_vsvimrc \;
+vsvimrc="$(dirname $(readlink -f $0))/.wsl.vsvimrc"
+cp $vsvimrc $WINUSERP/_vsvimrc
 
