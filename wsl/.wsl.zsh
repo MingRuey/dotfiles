bdf86dceee7a38ff32ab3b32e1540b895cc35366
mrchou <imchou239@gmail.com>
2021-01-19 17:46:45 +0800

    Attempt to switch from VS to Rider

--- wsl/.wsl.zsh
+++ wsl/.wsl.zsh
@@ -5,7 +5,9 @@ echo "Config WSL Zsh Settings (.wsl.zsh)"
 # copy vimrc file for VsVim (vim emulator for Visual Studio)
 WINUSERP=$(wslpath $(wslvar USERPROFILE))
 vsvimrc="$(dirname $(readlink -f $0))/.wsl.vsvimrc"
+ideavimrc="$(dirname $(readlink -f $0))/.wsl.ideavimrc"
 cp $vsvimrc $WINUSERP/_vsvimrc
+cp $ideavimrc $WINUSERP/_ideavimrc
 
 # source wsl specific local shortchuts
 . "$(dirname $(readlink -f $0))/.apps"
