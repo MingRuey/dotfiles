e791c6fc5196e619ea823ae77a405e552578df5a
mrchou <imchou239@gmail.com>
2021-01-19 17:51:48 +0800

    Change default git branch from master to main

--- git/install.sh
+++ git/install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GITTEMPDIR="$HOME/.git-template"
 mkdir -p $GITTEMPDIR
+
+# install default template
+if [[ -f "$GITTEMPDIR/HEAD" ]]
+then
+    echo "$GITTEMPDIR/HEAD exist, overwrite it."
+    rm "$GITTEMPDIR/HEAD"
+fi
+
+cp "$DIR/HEAD" "$GITTEMPDIR"
 
 # install hooks
 if [[ -d "$GITTEMPDIR/hooks" ]]
@@ -9,8 +19,6 @@ then
     echo "$GITTEMPDIR/hooks exists, remove it."
     rm -r "$GITTEMPDIR/hooks"
 fi
-
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo -e "Copy $DIR/hooks into $GITTEMPDIR"
 cp -r "$DIR/hooks" $GITTEMPDIR
