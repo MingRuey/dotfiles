7f0555f04222667515456152e8e05bb95f4d2f4f
mrchou <imchou239@gmail.com>
2021-09-23 16:40:42 -0400

    Also copy ideavimrc for linux

--- wsl/.wsl.zsh
+++ wsl/.wsl.zsh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "Config WSL Zsh Settings (.wsl.zsh)"
-
 # copy vimrc file for VsVim (vim emulator for Visual Studio)
 WINUSERP=$(wslpath $(wslvar USERPROFILE))
 vsvimrc="$(dirname $(readlink -f $0))/.wsl.vsvimrc"
 ideavimrc="$(dirname $(readlink -f $0))/.wsl.ideavimrc"
 cp $vsvimrc $WINUSERP/_vsvimrc
 cp $ideavimrc $WINUSERP/_ideavimrc
+cp $ideavimrc ~/.ideavimrc
 
 # source wsl specific local shortchuts
 . "$(dirname $(readlink -f $0))/.apps"
+
+echo "Config WSL Zsh Settings (.wsl.zsh)"
