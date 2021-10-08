a544bf490599f7c4c9e999df72a18d7cc3ec4764
mrchou <imchou239@gmail.com>
2021-10-08 11:16:53 -0400

    Include xclip into standard install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Required packages:
-aptgetPKGS="zsh zsh-doc autojump taskwarrior"
+aptgetPKGS="zsh zsh-doc autojump taskwarrior xclip"
 
 echo "Check if $aptgetPKGS installed:"
 sudo apt-get install -y $aptgetPKGS
